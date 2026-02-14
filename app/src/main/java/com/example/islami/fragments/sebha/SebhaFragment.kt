1956088a368f6f654aa35146063b866e43483b1a
Ezat Rashad <ezatrashad2003@gmail.com>
2026-02-14 22:46:06 +0200

    add animation to sebha

--- app/src/main/java/com/example/islami/fragments/sebha/SebhaFragment.kt
+++ app/src/main/java/com/example/islami/fragments/sebha/SebhaFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.RotateAnimation
 import androidx.fragment.app.Fragment
 import com.example.islami.databinding.FragmentSebhaBinding
 
@@ -12,6 +13,13 @@ class SebhaFragment : Fragment() {
     private lateinit var binding: FragmentSebhaBinding
 
     private var count = 0
+    private var zekrIndex = 0
+
+    private val azkarList = listOf(
+        "سبحان الله",
+        "الحمد لله",
+        "الله أكبر"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,24 +31,67 @@ class SebhaFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        updateCounter()
+
+        updateUI()
         setupClicks()
     }
 
     private fun setupClicks() {
 
-        binding.sebhaCount.setOnClickListener {
+        binding.sebhaCountBody.setOnClickListener {
+
             count++
-            updateCounter()
+
+
+            if (count > 33) {
+                moveToNextZekr()
+            }
+
+            updateUI()
+            rotateSebha()
         }
 
         binding.resetBtn.setOnClickListener {
             count = 0
-            updateCounter()
+            zekrIndex = 0
+            updateUI()
         }
     }
 
-    private fun updateCounter() {
-        binding.sebhaCount.text = count.toString()
+
+    private fun updateUI() {
+        binding.sebhaCountText.text = count.toString()
+        binding.zekrText.text = azkarList[zekrIndex]
+    }
+
+
+    private fun moveToNextZekr() {
+
+        count = 1
+
+        zekrIndex++
+
+
+        if (zekrIndex >= azkarList.size) {
+            zekrIndex = 0
+        }
+    }
+
+
+    private fun rotateSebha() {
+
+        val rotate = RotateAnimation(
+            0f,
+            360f,
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f,
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f
+        )
+
+        rotate.duration = 300
+        rotate.fillAfter = true
+
+        binding.sebhaCountBody.startAnimation(rotate)
     }
 }
