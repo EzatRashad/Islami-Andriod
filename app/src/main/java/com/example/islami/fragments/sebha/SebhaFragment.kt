fc299f6368c8b0ee803bf77055d3a7c744769820
Ezat Rashad <ezatrashad2003@gmail.com>
2026-02-14 18:21:43 +0200

    sebha fragment task done

--- app/src/main/java/com/example/islami/fragments/sebha/SebhaFragment.kt
+++ app/src/main/java/com/example/islami/fragments/sebha/SebhaFragment.kt
@@ -8,11 +8,14 @@ import androidx.fragment.app.Fragment
 import com.example.islami.databinding.FragmentSebhaBinding
 
 class SebhaFragment : Fragment() {
+
     private lateinit var binding: FragmentSebhaBinding
 
+    private var count = 0
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSebhaBinding.inflate(inflater, container, false)
@@ -20,8 +23,24 @@ class SebhaFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        updateCounter()
+        setupClicks()
     }
 
+    private fun setupClicks() {
 
+        binding.sebhaCount.setOnClickListener {
+            count++
+            updateCounter()
+        }
+
+        binding.resetBtn.setOnClickListener {
+            count = 0
+            updateCounter()
+        }
+    }
+
+    private fun updateCounter() {
+        binding.sebhaCount.text = count.toString()
+    }
 }
